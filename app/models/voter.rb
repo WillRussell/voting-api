1f30b6b20604635351e2ef015d5f263ae053768e
Will <wrussell35@gmail.com>
2014-11-04 17:31:45 -0500

    json formatting added

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,4 +1,28 @@
+require 'securerandom'
+
 class Voter < ActiveRecord::Base
 	validates :name, presence: true
 	validates :party, presence: true, uniqueness: true
+
+	before_save :add_token
+
+
+	def as_json(options)
+    hash = {id: id,
+            name: name,
+            party: party}
+    if options[:include_token]
+      hash.merge!(token: token)
+    end
+
+    {voter: hash}
+  end
+
+
+
+	private
+
+	def add_token
+		self.token ||= SecureRandom.uuid
+	end
 end
