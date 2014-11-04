81172c7a571e039b0e5ad12d227839880a2a0a5f
Will <wrussell35@gmail.com>
2014-11-04 17:21:58 -0500

    model validations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,4 @@
 class Candidate < ActiveRecord::Base
+	validates :name, presence: true
+	validates :party, presence: true, uniqueness: true
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,2 +1,4 @@
 class Voter < ActiveRecord::Base
+	validates :name, presence: true
+	validates :party, presence: true, uniqueness: true
 end
