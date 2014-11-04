2b7f10ec827a485efa36c9aeb9af890ead43488a
Will <wrussell35@gmail.com>
2014-11-04 16:40:46 -0500

    routes finally fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :voters, only: [:create, :index, :show, :update]
-      resources :candidates
+      resources :candidates, only: [:index, :show]
     end
   end
 
